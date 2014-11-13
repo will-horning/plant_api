e6d378637fb13fc24f7e2896bbd4e6854f3c463e
Will <will.horning@gmail.com>
2014-11-12 23:09:14 -0500

    Moved assets to seperate file.

---
+++ plant_api/app.py
@@ -0,0 +1,18 @@
+import pymongo, os
+from subprocess import check_output
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+mongo = PyMongo()
+
+
+def create_app(config={}):
+    app = Flask(__name__)
+    app.config.update(config)
+    app.config['MONGO_URI'] = os.environ['MONGOLAB_TEST_URI'] 
+    mongo.init_app(app)
+    from assets import assets
+    assets.init_app(app)
+    from views import api_blueprint
+    app.register_blueprint(api_blueprint)
+    return app
+
---
+++ plant_api/assets.py
@@ -0,0 +1,18 @@
+from flask.ext.assets import Environment, Bundle
+from webassets.filter import Filter, register_filter
+
+class CoffeeifyFilter(Filter):
+    name = 'coffeeify'
+
+    def input(self, _in, out, **kwargs):
+        path = kwargs['source_path']
+        out.write(check_output(['browserify', '-t', 'coffeeify', path]))
+
+    def output(self, _in, out, **kwargs):
+        out.write(_in.read())
+
+register_filter(CoffeeifyFilter)
+
+js = Bundle('coffee/main.coffee', filters=['coffeeify'], output='main.js')
+assets = Environment()
+assets.register('js_all', js)
